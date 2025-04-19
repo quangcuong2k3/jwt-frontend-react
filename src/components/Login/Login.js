2bdc4d587fd40c95b939315dfd097cfc47bbd52e
Cuong <cuongquangle539@gmail.com>
2025-04-19 21:37:31 +0700

    9.2

---
+++ src/components/Login/Login.js
@@ -0,0 +1,9 @@
+const Login = (props) => {
+    return (
+        <div className="login-container">
+            Login
+            <button>Text</button>
+        </div>
+    )
+};
+export default Login;
